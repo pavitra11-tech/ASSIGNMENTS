5edc0ea0ffbc6e711136cc2de495d49fd2b58e6b
Pavitra <study.pavitra1101@gmail.com>
2025-08-27 09:55:47 -0400

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,20 @@
+#Addition of 2 numbers
+number1= float(input("Enter the number1:"))
+number2= float(input("Enter the number2:"))
+Addition= number1+number2
+print('Addition:', Addition)
+Subtraction= number1-number2
+print('Subtraction:', Subtraction)
+Multiplication= number1*number2
+print('Multiplication:', Multiplication)
+Division= number1/number2
+print('Division:', Division)
+
+
+#Greeting message 
+FirstName = str(input("Enter your first name: "))
+LastName = str(input("Enter your last name:"))
+
+FullName = FirstName + " " + LastName 
+GreetingMessage= "Hello, " + FullName + "! Welcome to the Python's World"
+print("GreetingMessage", GreetingMessage)
